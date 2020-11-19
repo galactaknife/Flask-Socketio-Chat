4f633007781b4d3e1b47cc75e2430d3178cdb488
galactaknife <74742492+galactaknife@users.noreply.github.com>
2020-11-19 14:40:19 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Flask Web Chat",
+    version="0.0.1",
+    author="Galacta Knife",
+    author_email="galactaknife@gmail.com",
+    description="Flask Web Chat",
+    long_description="Flask Web Chat",
+    long_description_content_type="text/markdown",
+    url="https://github.com/galactaknife/Flask-Socketio-Chat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
