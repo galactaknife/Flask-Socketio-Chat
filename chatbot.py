16b201bbdadd521ceb0d641cae87b660965e2b6a
galactaknife <74742492+galactaknife@users.noreply.github.com>
2020-11-28 08:27:15 -0800

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,78 @@
-import datetime
+import datetime, random
+combinations = []
 
-def analyze(message):
-    message.strip("//")
+# If command is detected, analyze message using chatbot
+def analyze(message, name):
+    message.strip("//").lower()
+    # Gets time
     if "time" in message:
-        return datetime.datetime.now().strftime("%a, %b %d %Y, %I:%M:%S %p")
+        return str(datetime.datetime.now().strftime("%a, %b %d, %Y, %I:%M:%S %p"))
+    # Does combination
+    elif "combo" in message:
+        if "&" in message:
+            msg = str(message).replace("combo", '').replace("//", '').replace(' ', '')
+            msg = list(msg.split('&'))
+            return comboCalc(msg[0], msg[1])
+        else:
+            return "Missing '&'"
+    # Gets random number between parameters
+    elif "random" in message:
+        if "&" in message:
+            msg = str(message).replace("random", '').replace("//", '').replace(' ', '')
+            msg = list(msg.split('&'))
+            try:
+                new = [int(msg[0]), int(msg[1])]
+                print(new)
+                if abs(max(new) - min(new)) >= 500:
+                    return "Max number difference is 500.  Your difference is " + str(abs(max(new) - min(new)))
+                else:
+                    return str(random.choice(list(range(min(new), max(new)))))
+            except:
+                return "Enter valid numbers"
+        else:
+            return "Missing '&'"
+    elif "about" in message and "you" in message:
+        return "I, chatbot, was created to monitor this chat"
+    elif "joke" in message and "tell" in message:
+        return random.choice(["Your life", "When life gives you melons, you're probably dyslexic", "I asked my North Korean friend how it was up there, he said he couldn't complain.", "Whiteboards are remarkable", "I want to die peacefully like my grandfather.  Not screaming and yelling like the passengers in his car.", "I have the heart of a lion and a lifetime ban from the Toronto Zoo", "An apple a day keeps the doctor away, only if you aim it well."])
+    # Sends image
+    elif "image" in message:
+        msg = str(message).replace("image", '').replace("//", '', 1).replace(' ', '')
+        return "<img width='200' src='"+ msg +"' alt='Image not found'>"
+    # Changes display settings
+    elif "change" in message:
+        msg = list(message.split('='))
+        # Sets settings to default
+        if not len(msg) == 2:
+            if "default" in message:
+                return "<style>body {color: black !important; background-color: white;} #messages p:nth-child(even) {background-color: #ededed;} #sendArea {background-color: white !important; color: black;} td, th { border: 1px solid black;} #messages p { color: black; font-family: Helvetica; font-size: 13px;} #uploadImage { filter: invert(0%); }</style>Reset your settings to default", "update"
+            return "Specify a valid changable element", "update"
+        # Changes values
+        elif "color" in message:
+            return "<style>#messages p { color: " + str(msg[1]) + ";}</style>Your color is now " + str(msg[1]), "update"
+        elif "font-size" in message:
+            try:
+                return "<style>#messages p { font-size:" + str(int(msg[1])) + "px;}</style>Your font size is now " + str(msg[1]), "update"
+            except:
+                return "Invalid font size", "update"
+        elif "font" in message:
+            return "<style>#messages p { font-family:" + str(msg[1]) + ";}</style>Your font is now " + str(msg[1]), "update"
+        # Changes theme
+        elif "theme" in message:
+            if "dark" in message:
+                return "<style>body {color: gainsboro !important; background-color: #1f1f1f} #messages p:nth-child(even) {background-color: #3b3b3b;} #sendArea {background-color: #4a4a4a !important; color: white;} td, th { border: 1px solid gainsboro;} #messages p { color: white;} #uploadImage { filter: invert(100%); }</style>Your theme is now dark", "update"
+            elif "light" in message:
+                return "<style>body {color: black !important; background-color: white} #messages p:nth-child(even) {background-color: #ededed;} #sendArea {background-color: white !important; color: black;} td, th { border: 1px solid black;} #messages p { color: black;} #uploadImage { filter: invert(0%); }</style>Your theme is now light", "update"
+            return "Invalid theme.  Valid themes are dark and light"
+        return "Specify a valid changable element", "update"
     return "Not a command"
+
+def comboCalc(name1, name2):
+  # Checks if combination already exists
+  for combo in combinations:
+    if name1 in combo["names"] and name2 in combo["names"]:
+      return "Match data for" + combo["names"][0] + " and " + combo["names"][1] + ": " + str(combo["calc"]) + '%'
+  # If not, create new combination
+  new = {"names" : [name1, name2], "calc" : random.choice(list(range(0, 100)))}
+  combinations.append(new)
+  return "Match data for " + new["names"][0] + " and " + new["names"][1] + ": " + str(new["calc"]) + '%'
--- main.py
+++ main.py
@@ -1,73 +1,242 @@
 from flask import Flask, redirect, url_for, render_template, request, session, Markup
-from flask_socketio import SocketIO, send, emit
-import random
+from flask_socketio import SocketIO, emit, join_room, leave_room, disconnect
+import random, json
+import eventlet
+from eventlet import wsgi
 import chatbot as cb
 
+# Initialize app
 app = Flask(__name__)
 app.secret_key = "doodoocaca"
 socketio = SocketIO(app)
 
-users = {}
-colors = {}
+# Holds all rooms and room data
+rooms = {
+    "Room 1" : {
+        "users" : {},
+        "colors" : {},
+        "banned" : []
+    },
+    "Room 2" : {
+        "users" : {},
+        "colors" : {},
+        "banned" : []
+    },
+    "Room 3" : {
+        "users" : {},
+        "colors" : {},
+        "banned" : []
+    }
+}
+
+# Holds name of added rooms
+addedRooms = []
+# Holds name of user and what room they're in
+userRoom = {}
 
 @app.route('/')
 def index():
-    if "user" in session:
-        user = session["user"]
-        colors[user] = random.choice(["#3399ff", "#953d96", "#e0383e", "#62ba46", "#f7821b", "lightSeaGreen", "steelBlue", "darkGoldenrod", "orangeRed", "#a83299", "#246b62", "teal", "royalBlue", "saddleBrown", "crimson", "green", "#607d46", "indianRed", "mediumVioletRed", "darkOrange", "olive", "gray", "mediumPurple", "#71268c"])
-        return render_template("index.html", username=Markup("<span class='name' style='background-color: " + colors[user] + "'>" + user + "</span>"))
-    else:
+    try:
+        # If user is logged in, send to chat page
+        if "user" in session:
+            # Initialize chat page
+            user = session["user"]
+            if user == "Admin":
+                rooms[userRoom[session["user"]]]["colors"][user] = "darkred"
+            else:
+                rooms[userRoom[session["user"]]]["colors"][user] = random.choice(["#3399ff", "#953d96", "#e0383e", "#62ba46", "#f7821b", "lightSeaGreen", "steelBlue", "darkGoldenrod", "orangeRed", "#a83299", "#246b62", "teal", "royalBlue", "saddleBrown", "crimson", "green", "#607d46", "indianRed", "mediumVioletRed", "darkOrange", "olive", "gray", "mediumPurple", "#71268c"])
+            return render_template("index.html", username=Markup("<span class='name' style='background-color: " + rooms[userRoom[session["user"]]]["colors"][user] + "'>" + user + "</span>"), room=userRoom[session["user"]])
+        # If user isn't logged in, redirect to login
+        else:
+            return redirect(url_for("login"))
+    except:
         return redirect(url_for("login"))
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    # If user logged in, get data and redirect to chat page
     if request.method == "POST":
-        user = request.form["nm"]
-        if user in users.values():
-            return render_template("login.html", flash="Username taken")
+        user = request.form["nm"].replace('@', '').replace(' ', '_')
+        # If username is Admin, get password
+        if user == "Admin":
+            try:
+                if request.form["pass"]:
+                    # If valid password, redirect to chat
+                    if not request.form["pass"] == "admin password":
+                        return render_template("login.html", rooms=rooms.keys(), passReq=user)
+                else:
+                    return render_template("login.html", rooms=rooms.keys())
+            except:
+                return render_template("login.html", rooms=rooms.keys(), passReq=user)
+        # If a user exists with username, redirect back to login
+        if user in userRoom.keys() or user in rooms.keys() or user == "chatbot":
+            return render_template("login.html", rooms=rooms.keys(), flash="Username taken")
+        userRoom[user] = request.form["rooms"]
         session["user"] = user
         return redirect(url_for("index"))
+    # Redirect to login if login form not submitted
     else:
-        try:
-            if request.sid in users:
-                return redirect(url_for("index"))
-        except:
-            return render_template("login.html")
+        return render_template("login.html", rooms=rooms.keys())
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
-    leave()
+    # If user logs out, redirect to login page
     return redirect(url_for("login"))
 
 @socketio.on("login")
 def loginMessage(user):
-    emit("message", user + "logged in", broadcast=True, namespace='/')
+    # If user logs in, send login message
+    try:
+        emit("message", user + "logged in", broadcast=True, namespace='/', room=userRoom[session["user"]])
+    except:
+        pass
 
 @socketio.on("connect")
 def connect():
-    for i in users.values():
-        if session["user"] == i:
-            del users[list(users.keys())[list(users.values()).index(session["user"])]]
-    users[request.sid] = session["user"]
-    emit("users", [list(users.values()), colors], broadcast=True, namespace='/')
+    # When user joins, reset all past data if applicable
+    try:
+        if session["user"] in rooms[userRoom[session["user"]]]["users"].values():
+            del rooms[userRoom[session["user"]]]["users"][list(rooms[userRoom[session["user"]]]["users"].keys())[list(rooms[userRoom[session["user"]]]["users"].values()).index(session["user"])]]
+    except:
+        pass
+    # Joins room and initializes user
+    try:
+        join_room(userRoom[session["user"]])
+        join_room(session["user"])
+        rooms[userRoom[session["user"]]]["users"][request.sid] = session["user"]
+        emit("users", [list(rooms[userRoom[session["user"]]]["users"].values()), json.dumps(rooms[userRoom[session["user"]]]["colors"])], broadcast=True, namespace='/', room=userRoom[session["user"]])
+    except:
+        emit("alert", "Session expired.  Reload page to send messages.", namespace='/')
 
 @socketio.on("disconnect")
 def leave():
-    session.pop("user", None)
+    # Delete user from room and remove session
     try:
-        users.pop(request.sid)
+        emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], "left the chat"], broadcast=True, namespace='/', room=userRoom[session["user"]])
+        room = userRoom[session["user"]]
+        leave_room(userRoom[session["user"]])
+        leave_room(session["user"])
+        try:
+            del rooms[userRoom[session["user"]]]["colors"][session["user"]]
+        except:
+            pass
+        rooms[userRoom[session["user"]]]["users"] = {key:val for key, val in rooms[userRoom[session["user"]]]["users"].items() if val != session["user"]}
+        emit("users", [list(rooms[userRoom[session["user"]]]["users"].values()), json.dumps(rooms[userRoom[session["user"]]]["colors"])], broadcast=True, namespace='/', room=room)
+        del userRoom[session["user"]]
+        session.pop("user", None)
     except:
         pass
-    emit("users", [list(users.values()), colors], broadcast=True, namespace='/')
 
 @socketio.on("message")
-def loginMessage(message):
-    if request.sid in users:
-        emit("message", [colors[session["user"]], session["user"], message], broadcast=True, namespace='/')
-        if "//" in message:
-            emit("message", ["chatbot", cb.analyze(message)], broadcast=True, namespace='/')
+def message(message):
+    try:
+        # If user is banned, don't allow message
+        if session["user"] in rooms[userRoom[session["user"]]]["users"].values():
+            if session["user"] in rooms[userRoom[session["user"]]]["banned"]:
+                emit("message", ["chatbot", "You are banned from " + userRoom[session["user"]]], namespace='/', room=session["user"])
 
-    else:
-        return redirect(url_for("login"))
+            # Sends private message if applicable
+            elif '@' in message:
+                sendee = list(message.split(' '))
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message, "private"], broadcast=True, namespace='/', room=session["user"])
+                sentTo = []
+                for i in sendee:
+                    i = i.replace(',', '')
+                    try:
+                        if i[0] == '@':
+                            if i not in sentTo and not i[1:] == session["user"]:
+                                if i[1:] in userRoom.keys() and not i[1:] in rooms[userRoom[session["user"]]]["users"].values():
+                                    sentTo.append(i)
+                                    emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message, "private", userRoom[session["user"]]], namespace='/', room=i[1:])
+                                elif i[1:] in userRoom.keys():
+                                    sentTo.append(i)
+                                    emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message, "private"], broadcast=True, namespace='/', room=i[1:])
+                                else:
+                                    emit("message", ["darkslategray", "chatbot", "User " + i[1:] + " doesn't exist"], broadcast=True, namespace='/', room=session["user"])
+                    except:
+                        pass
 
-socketio.run(host='0.0.0.0', port=PORT, debug=False)
+            # Adds room prompted by Admin
+            elif session["user"] == "Admin" and "// new room=" in message.lower():
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                newRoom = list(message.split('='))[1]
+                if not newRoom in addedRooms:
+                    addedRooms.append(newRoom)
+                    rooms[newRoom] = {"users" : {}, "colors" : {}, "banned" : []}
+                    emit("message", ["chatbot", "Created room " + newRoom], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                else:
+                    emit("message", ["chatbot", "Room " + newRoom + " already exists"], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+            # Deletes room prompted by Admin
+            elif session["user"] == "Admin" and "// del room=" in message.lower():
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                delRoom = list(message.split('='))[1]
+                if delRoom in addedRooms:
+                    addedRooms.remove(delRoom)
+                    del rooms[delRoom]
+                    emit("message", ["chatbot", "Deleted room " + delRoom], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                    emit("alert", "This chat room has been deleted by " + session["user"], broadcast=True, namespace='/', room=delRoom)
+                else:
+                    emit("message", ["chatbot", "Can't delete room " + delRoom], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+            # Views all bans
+            elif session["user"] == "Admin" and "// bans" in message:
+                bannedStr = 'Banned users: '
+                for i in rooms[userRoom[session["user"]]]["banned"]:
+                    bannedStr += i + ' '
+                if bannedStr == '':
+                    bannedStr = "Currently, there are no banned users"
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                emit("message", ["chatbot", bannedStr], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+            # Bans user if applicable
+            elif session["user"] == "Admin" and "// ban" in message and not "Admin" in message:
+                userToBan = message.replace('// ban', '').strip()
+                rooms[userRoom[session["user"]]]["banned"].append(userToBan)
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                emit("message", ["chatbot", userToBan + " has been banned from " + userRoom[session["user"]] + " by " + session["user"]], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+            # Unbans user if applicable
+            elif session["user"] == "Admin" and "// unban" in message:
+                userToUnban = message.replace('// unban', '').strip()
+                try:
+                    rooms[userRoom[session["user"]]]["banned"].remove(userToUnban)
+                    emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                    emit("message", ["chatbot", userToUnban + " has been unbanned from " + userRoom[session["user"]] + " by " + session["user"]], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                except:
+                    emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                    emit("message", ["chatbot", Markup("User <em>" + userToUnban + "</em> isn't banned")], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+            # Clears all bans if applicable
+            elif session["user"] == "Admin" and "// clearban" in message:
+                rooms[userRoom[session["user"]]]["banned"] = []
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                emit("message", ["chatbot", "All bans cleared by " + session["user"]], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+            # If command call, call chatbot
+            elif "//" == message[0:2]:
+                command = cb.analyze(message, session["user"])
+                if len(command) == 2 and command[1] == "update":
+                    emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message])
+                    emit("message", ["chatbot", Markup(command[0])], namespace='/')
+                else:
+                    emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+                    emit("message", ["chatbot", Markup(command)], broadcast=True, namespace='/', room=userRoom[session["user"]])
+            # If no special conditions, emit regular message to all users
+            else:
+                emit("message", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], message], broadcast=True, namespace='/', room=userRoom[session["user"]])
+        else:
+            return redirect(url_for("login"))
+    # If user disconnected, alert them
+    except:
+        emit("alert", "Session expired.  Reload page to send messages.", namespace='/')
+        disconnect()
+
+# Handles images
+@socketio.on("image")
+def image(data):
+    emit("imageBrodcast", [rooms[userRoom[session["user"]]]["colors"][session["user"]], session["user"], data], broadcast=True, namespace='/', room=userRoom[session["user"]])
+
+# Run app
+wsgi.server(eventlet.listen(('', 5000)), app)
+#socketio.run(app)
